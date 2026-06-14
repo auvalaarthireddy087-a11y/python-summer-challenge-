ad661ea2e159036f24323fea80ee01f605e00447
Arthi Reddy <arthireddy@gmail.com>
2026-06-14 13:13:43 +0530

    Remove Day-04 and Day-05

--- Day-05/Q1.py
+++
@@ -1,8 +0,0 @@
-def sum_of_natural_numbers(N):
-    total_sum = 0
-    for i in range(1, N + 1):
-        total_sum += i
-    return total_sum
-
-result = sum_of_natural_numbers(10)
-print(f"Sum of natural numbers up to 10 is {result}")
--- Day-05/Q2.py
+++
@@ -1,7 +0,0 @@
-def multiplication_table(N):
-    for i in range(1, 11):
-        product = N * i
-        print(f"{N} x {i} = {product}")
-
-print("Multiplication table for 5:")
-multiplication_table(5)
