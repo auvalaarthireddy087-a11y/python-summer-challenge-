35332081698466b96180fa6be17841017be0fccd
Arthi Reddy <arthireddy@gmail.com>
2026-06-19 10:20:11 +0530

    Add Day-10 solutions

---
+++ Day-10/Q2.py
@@ -0,0 +1,10 @@
+n, m = map(int,input.split())
+arr = []
+for i in range(n):
+    row = list(map(int, input().split()))
+    arr.append(rows)
+    for i in range(n):
+        s = 0
+        for j in range(m):
+            s+ = arr[i][j]
+print("sum of rows", i,"=",s)
---
+++ Day-10/Q3.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+arr = []
+for i in range(n):
+    row = list(map(int, input().split()))
+    arr.append(row)
+    key = int(input())
+    found = False
+    for i in range(n):
+        if arr[i][j] == key:
+            print("Found at:", i,j)
+            found = True
+            break
+        if found:
+            break 
+        if not found:
+            print("Not found")
+            
