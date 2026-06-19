e0bdb1e697dde8cd19ea2f2c6a33193790190c12
Arthi Reddy <arthireddy@gmail.com>
2026-06-19 10:25:25 +0530

    Fix Day-10 Q2 Q3 syntax and logic

--- Day-10/Q2.py
+++ Day-10/Q2.py
@@ -1,10 +1,10 @@
-n, m = map(int,input.split())
+n, m = map(int, input().split())
 arr = []
 for i in range(n):
     row = list(map(int, input().split()))
-    arr.append(rows)
-    for i in range(n):
-        s = 0
-        for j in range(m):
-            s+ = arr[i][j]
-print("sum of rows", i,"=",s)
+    arr.append(row)
+for i in range(n):
+    s = 0
+    for j in range(m):
+        s += arr[i][j]
+    print("sum of rows", i, "=", s)
--- Day-10/Q3.py
+++ Day-10/Q3.py
@@ -3,15 +3,16 @@ arr = []
 for i in range(n):
     row = list(map(int, input().split()))
     arr.append(row)
-    key = int(input())
-    found = False
-    for i in range(n):
+key = int(input())
+found = False
+for i in range(n):
+    for j in range(m):
         if arr[i][j] == key:
-            print("Found at:", i,j)
+            print("Found at:", i, j)
             found = True
             break
-        if found:
-            break 
-        if not found:
-            print("Not found")
+    if found:
+        break
+if not found:
+    print("Not found")
             
